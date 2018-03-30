88cbc2ac9965a7fed4bd3c488b202f888db8a4d7
Monika Zietal <monika.zietal01@gmail.com>
2018-03-30 21:42:59 +0200

    feat: Add list of cards

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
  * Create a list that holds all of your cards
  */
 
+ let cardsList = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
+
 
 /*
  * Display the cards on the page
